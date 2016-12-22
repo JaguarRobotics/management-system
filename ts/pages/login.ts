07b4cdcc50f5e6d0053bd6030de40a11001c49a8
Zach Deibert <zachdeibert@gmail.com>
2016-12-22 17:34:52 -0600

    Added more navigation code

--- ts/pages/login.ts
+++ ts/pages/login.ts
@@ -1,9 +1,10 @@
 /// <reference path="../page.ts" />
+/// <reference path="../navigation.ts" />
 
 namespace org.usd232.robotics.management.pages {
     export class LoginController extends AbstractPage {
         protected init(): void {
-            
+            this.$scope.create = () => HistoryController.load("/create");
         }
     }
 }
