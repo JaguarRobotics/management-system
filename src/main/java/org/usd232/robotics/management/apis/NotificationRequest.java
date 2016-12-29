134f1051d6f630e91bc256728bb40eb2fce75dfb
Zach Deibert <zachdeibert@gmail.com>
2016-12-29 10:18:39 -0600

    Generated some code

--- src/main/java/org/usd232/robotics/management/apis/NotificationRequest.java
+++ src/main/java/org/usd232/robotics/management/apis/NotificationRequest.java
@@ -1,5 +1,7 @@
 package org.usd232.robotics.management.apis;
 
+import java.io.Serializable;
+
 /**
  * The request to send a notification
  * 
@@ -7,8 +9,9 @@ package org.usd232.robotics.management.apis;
  * @since 1.0
  * @version 1.0
  */
-public class NotificationRequest
+public class NotificationRequest implements Cloneable, Serializable, Comparable<NotificationRequest>
 {
+    private static final long serialVersionUID = 6211783957529837753L;
     /**
      * The message to send
      * 
@@ -21,4 +24,109 @@ public class NotificationRequest
      * @since 1.0
      */
     public NotificationTarget target;
+
+    @Override
+    public NotificationRequest clone()
+    {
+        return new NotificationRequest(this);
+    }
+
+    @Override
+    public int hashCode()
+    {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((message == null) ? 0 : message.hashCode());
+        result = prime * result + ((target == null) ? 0 : target.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj)
+    {
+        if (this == obj)
+        {
+            return true;
+        }
+        if (obj == null)
+        {
+            return false;
+        }
+        if (!(obj instanceof NotificationRequest))
+        {
+            return false;
+        }
+        NotificationRequest other = (NotificationRequest) obj;
+        if (message == null)
+        {
+            if (other.message != null)
+            {
+                return false;
+            }
+        }
+        else if (!message.equals(other.message))
+        {
+            return false;
+        }
+        if (target == null)
+        {
+            if (other.target != null)
+            {
+                return false;
+            }
+        }
+        else if (!target.equals(other.target))
+        {
+            return false;
+        }
+        return true;
+   }
+
+    @Override
+    public int compareTo(NotificationRequest o)
+    {
+        return ((Integer) hashCode()).compareTo(o.hashCode());
+    }
+
+    @Override
+    public String toString()
+    {
+        return String.format("NotificationRequest [message=%s, target=%s]", message, target);
+    }
+
+    /**
+     * Nullary constructor
+     *
+     * @since 1.0
+     */
+    public NotificationRequest()
+    {
+    }
+
+    /**
+     * Default constructor
+     *
+     * @param message
+     *            The message to send      
+     * @param target
+     *            The target to send the message to      
+     * @since 1.0
+     */
+    public NotificationRequest(String message, NotificationTarget target)
+    {
+        this.message = message;
+        this.target = target;
+    }
+
+    /**
+     * Copy constructor
+     *
+     * @param obj
+     *            The object to copy from
+     * @since 1.0
+     */
+    public NotificationRequest(NotificationRequest obj)
+    {
+        this(obj.message, obj.target);
+    }
 }
--- src/main/java/org/usd232/robotics/management/apis/permissions/AttendancePermissions.java
+++ src/main/java/org/usd232/robotics/management/apis/permissions/AttendancePermissions.java
@@ -1,5 +1,7 @@
 package org.usd232.robotics.management.apis.permissions;
 
+import java.io.Serializable;
+
 /**
  * The permissions relating to attendance records
  * 
@@ -7,8 +9,9 @@ package org.usd232.robotics.management.apis.permissions;
  * @since 1.0
  * @version 1.0
  */
-public class AttendancePermissions
+public class AttendancePermissions implements Cloneable, Serializable, Comparable<AttendancePermissions>
 {
+    private static final long serialVersionUID = 5289201128626483161L;
     /**
      * If the user can view attendance records
      * 
@@ -27,4 +30,103 @@ public class AttendancePermissions
      * @since 1.0
      */
     public boolean excuse;
+
+    @Override
+    public AttendancePermissions clone()
+    {
+        return new AttendancePermissions(this);
+    }
+
+    @Override
+    public int hashCode()
+    {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + Boolean.hashCode(view);
+        result = prime * result + Boolean.hashCode(modify);
+        result = prime * result + Boolean.hashCode(excuse);
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj)
+    {
+        if (this == obj)
+        {
+            return true;
+        }
+        if (obj == null)
+        {
+            return false;
+        }
+        if (!(obj instanceof AttendancePermissions))
+        {
+            return false;
+        }
+        AttendancePermissions other = (AttendancePermissions) obj;
+        if (view != other.view)
+        {
+            return false;
+        }
+        if (modify != other.modify)
+        {
+            return false;
+        }
+        if (excuse != other.excuse)
+        {
+            return false;
+        }
+        return true;
+   }
+
+    @Override
+    public int compareTo(AttendancePermissions o)
+    {
+        return ((Integer) hashCode()).compareTo(o.hashCode());
+    }
+
+    @Override
+    public String toString()
+    {
+        return String.format("AttendancePermissions [view=%s, modify=%s, excuse=%s]", view, modify, excuse);
+    }
+
+    /**
+     * Nullary constructor
+     *
+     * @since 1.0
+     */
+    public AttendancePermissions()
+    {
+    }
+
+    /**
+     * Default constructor
+     *
+     * @param view
+     *            If the user can view attendance records      
+     * @param modify
+     *            If the user can modify attendance records      
+     * @param excuse
+     *            If the user can excuse absences      
+     * @since 1.0
+     */
+    public AttendancePermissions(boolean view, boolean modify, boolean excuse)
+    {
+        this.view = view;
+        this.modify = modify;
+        this.excuse = excuse;
+    }
+
+    /**
+     * Copy constructor
+     *
+     * @param obj
+     *            The object to copy from
+     * @since 1.0
+     */
+    public AttendancePermissions(AttendancePermissions obj)
+    {
+        this(obj.view, obj.modify, obj.excuse);
+    }
 }
