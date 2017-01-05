d84d013ff8f22dbaf8212578c6be33e91539cfe6
Zach Deibert <zachdeibert@gmail.com>
2017-01-05 17:11:10 -0600

    Added qr code support to kiosk

--- compile.sh
+++ compile.sh
@@ -17,6 +17,15 @@ source ~/.rvm/scripts/rvm
 rvm use 2.3.0
 PATH=$PATH:node_modules/.bin/:$(bundle show jekyll)/exe/
 
+# Download external scripts
+wget https://github.com/andrastoth/webcodecamjs/archive/master.zip
+unzip master.zip
+rm master.zip
+mkdir -p audio
+cp webcodecamjs-master/audio/beep.mp3 audio/
+cp webcodecamjs-master/js/{qrcodelib,webcodecamjquery,DecoderWorker}.js js/
+rm -rf webcodecamjs-master
+
 # Build Jekyll content
 if [ $dev -eq 1 ]; then
     jekyll serve -H 0.0.0.0 | prefixOutput "[Jekyll]     " &
