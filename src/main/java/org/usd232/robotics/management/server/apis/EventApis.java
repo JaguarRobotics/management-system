32865a99ef8296ebac48d64f43a45af21b48a643
Zach Deibert <zachdeibert@gmail.com>
2017-01-06 07:58:51 -0600

    Added get event api to server

--- src/main/java/org/usd232/robotics/management/server/apis/EventApis.java
+++ src/main/java/org/usd232/robotics/management/server/apis/EventApis.java
@@ -4,13 +4,16 @@ import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Time;
 import java.util.ArrayList;
 import java.util.List;
 import org.usd232.robotics.management.apis.Event;
 import org.usd232.robotics.management.apis.EventSignup;
+import org.usd232.robotics.management.apis.EventTime;
 import org.usd232.robotics.management.apis.EventType;
 import org.usd232.robotics.management.server.database.Database;
 import org.usd232.robotics.management.server.routing.GetApi;
+import org.usd232.robotics.management.server.session.RequirePermissions;
 import org.usd232.robotics.management.server.session.Session;
 
 /**
@@ -53,6 +56,41 @@ public class EventApis
                                     attended == -1 ? null : attended == 1));
                 }
                 return events.toArray(new Event[0]);
+            }
+        }
+    }
+
+    /**
+     * Gets the information about a single event
+     * 
+     * @param url
+     *            The request url
+     * @return The event object
+     * @since 1.0
+     * @throws SQLException
+     *             If an error occurs while connecting to the database
+     */
+    @GetApi("/events/*")
+    @RequirePermissions("event.view")
+    public static Event getEvent(String url) throws SQLException
+    {
+        int eventId = Integer.parseInt(url.substring(8, url.length() - 5));
+        try (PreparedStatement st = Database.prepareStatement(
+                        "SELECT `type`, `name`, `location`, `date`, `start`, `end`, `signup` FROM `events` WHERE `id` = ?"))
+        {
+            st.setInt(1, eventId);
+            try (ResultSet res = st.executeQuery())
+            {
+                if (!res.next())
+                {
+                    return null;
+                }
+                Time start = res.getTime(5);
+                Time end = res.getTime(6);
+                Date deadline = res.getDate(7);
+                return new Event(eventId, EventType.valueOf(res.getString(1)), res.getString(2), res.getString(3),
+                                res.getDate(4), new EventTime(start == null && end == null, start, end),
+                                new EventSignup(deadline != null, deadline, null), null);
             }
         }
     }
